40c052ed2678ab70eb1ab70a91731fce3c134154
Keitaroh Kobayashi <keita@kkob.us>
2014-09-14 10:37:55 +0900

    Updates submodule to 9.7.1 (5.41.1)

--- lib/jquery.fileupload-rails.rb
+++ lib/jquery.fileupload-rails.rb
@@ -2,8 +2,8 @@ require "jquery-ui-rails"
 
 module JqueryFileUpload
   module Rails
-		class Rails::Engine < ::Rails::Engine
+    class Rails::Engine < ::Rails::Engine
       paths['vendor/assets'] << 'vendor/legacy_assets'
-		end
+    end
   end
 end
